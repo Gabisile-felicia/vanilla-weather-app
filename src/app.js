38d3ecb4df4dbdda57b5589ffcf05ab29abcda66
Gabisile-felicia <137975209+Gabisile-felicia@users.noreply.github.com>
2023-07-07 12:34:15 +0200

    added current temperature with the use of api..

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-alert("hello");
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let feelsElement = document.querySelector("#feels");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  feelsElement.innerHTML = Math.round(response.data.temperature.feels_like);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "80b9c172ft17b407e06ebbaadc3039o3";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=paris&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
