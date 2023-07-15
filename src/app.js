c64567fa90d723c56bbb69fdc1054b031d591053
Gabisile-felicia <137975209+Gabisile-felicia@users.noreply.github.com>
2023-07-15 11:13:48 +0200

    removed the html structure and put it via js through a function.

--- src/app.js
+++ src/app.js
@@ -22,6 +22,31 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img src="" alt="Clear" id="" />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max"> 20° </span>
+                <span class="weather-forecast-temperature-min"> 13° </span>
+              </div>
+            </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
@@ -61,3 +86,4 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Mbombela");
+displayForecast();
